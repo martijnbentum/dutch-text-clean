9085d098ea7b2a2c33958862fe3c9caa5cdbc808
martijnbentum <bentummartijn@gmail.com>
2026-01-23 18:09:43 +0100

    helper for cgn specific cleaning

---
+++ dutch_text_clean/cgn_utils.py
@@ -0,0 +1,46 @@
+cgn_clitic_map = {
+    "'k": "ik",
+    "'t": "het",
+    "'m": "hem",
+    "'r": "er",
+    "m'n": "mijn",
+    "z'n": "zijn",
+    "d'r": "haar",
+    "'s": "is",
+    "da's": "dat is",
+    "'tis": "het is",
+    "d'rin": "daarin",
+    "d'rop": "daarop",
+    "d'raan": "daaraan",
+    "'tzelfde": "hetzelfde",
+    "'tgeen": "hetgeen",
+    "'tgene": "hetgene",
+    "'tzelfste": "hetzelfde",
+    "'ns": "eens",
+    "ja'k": "ja ik",
+    "dat'k": "dat ik",
+    "ja'k": "ja ik",
+}
+
+star_list = ['*v', '*d', '*z', '*n', '*t', '*a', '*u', '*x']
+'''
+https://taalmaterialen.ivdnt.org/wp-content/uploads/documentatie/cgn_website/doc_Dutch/topics/index.htm
+ gebruik *v voor woorden uit een vreemde taal (bijv.: tomorrow*v) (zie ook C3);
+ gebruik *d voor dialectwoorden (bijv.: kortewagen*d; keuje*d) (zie ook D4);
+ gebruik *z voor woorden uit de standaardtaal die zwaar dialectisch zijn uitgesproken (zie D5);
+ gebruik *n voor nieuwe woorden (bijv.: zerotolerantie*n) (zie ook C4);
+ gebruik *t voor nieuwe tussenwerpsels (bijv.: amaai*t; hoppa*t) (zie ook C5);
+ gebruik *a voor afgebroken woorden (bijv.: uitges*a; verpr*a) (zie ook C6);
+ gebruik *u voor het weergeven van de uitspraak en versprekingen (bijv.: boink*u; gespreken*u) 
+ gebruik *x bij woorden waarvan u niet zeker bent of u ze goed heeft verstaan (zie C8).
+'''
+
+remove_words = ["ggg","uh","uhu","uhm","uh-hu","mm-hu","mmm","xxx","Xxx"]
+    
+    
+    
+    
+    
+    
+    
+    
